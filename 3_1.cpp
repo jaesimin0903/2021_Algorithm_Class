9239401c36f81edf0f394bd52efa66632dbeea15
Jaemin Choi <a980903@naver.com>
2021-03-26 16:14:29 +0900

    third -2

--- 3_1.cpp
+++ 3_1.cpp
@@ -4,6 +4,7 @@
 
 using namespace std;
 int data_Swap = 0;
+int compare = 0;
 void insertionSort(std::vector<int>& vec)
 {
     for (auto it = vec.begin(); it != vec.end(); it++)
@@ -17,39 +18,66 @@ void insertionSort(std::vector<int>& vec)
         std::rotate(insertion_point, it, it + 1);
         data_Swap++;
     }
-    
+
+}
+void insertion(vector<int>& a, int n)
+{
+    int i, j, v;
+
+    for (i = 2; i <= n; i++)
+    {
+        compare++;
+        v = a[i]; j = i;
+        while (a[j - 1] > v) { data_Swap++; a[j] = a[j - 1]; j--; }
+        a[j] = v;
+
+    }
+
 }
 
 // Function to print the array
-void print(std::vector<int> vec)
+void print(char A, int a, int b)
 {
-    for (int x : vec)
-        std::cout << x << " ";
-    std::cout << '\n';
+    cout << "Compare_Cnt_" << A << " " << a << ", " << "DataMove_Cnt_" << A << " " << b << endl;
 }
 
-// Driver code
+
 int main()
 {
+    int N;
+    int c1, c2, d1, d2;
+    cout << "N - ";
+    cin >> N;
+    int j = N;
     std::vector<int> arr, arr1;
-    for (int i = 20000; i > 0; i--) {
-        arr.push_back(i);
+    arr.resize(N, 0);
+    arr1.resize(N, 0);
+    for (int i = 0; i < N; i++) {
+        arr.push_back(j);
+        j--;
     }
-    insertionSort(arr);
- 
+    arr.erase(arr.begin(), arr.begin() + N - 1);
+    insertion(arr, N);
+
+    cout << "SortedData_A: ";
+    for (int i = 1; i <= 20; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    c1 = compare;
+    d1 = data_Swap;
+    data_Swap = 0;
+    compare = 0;
     srand((unsigned)time(NULL));
-    for (int i = 0; i < 20000; i++) {
+    for (int i = 0; i < N; i++) {
         int a = 1 + rand() % 100000;
         arr1.push_back(a);
     }
-
-    int k = 0;
-    insertionSort(arr1);
     unique(arr1.begin(), arr1.end());
     arr1.erase(unique(arr1.begin(), arr1.end()), arr1.end());
-    while (arr1.size() != 20000)
+    while (arr1.size() < N)
     {
-        for (int i = arr1.size(); i < 20000; i++) {
+        for (int i = arr1.size(); i < N; i++) {
             int j = arr1.size();
             int a = 1 + rand() % 100000;
             arr1.push_back(a);
@@ -57,8 +85,16 @@ int main()
             arr1.erase(unique(arr1.begin(), arr1.end()), arr1.end());
         }
     }
-
-    for (int i = 0; i < 20; i++) {
+    insertion(arr1, N);
+    unique(arr1.begin(), arr1.end());
+    arr1.erase(arr1.begin() + N, arr1.end());
+    c2 = compare;
+    d2 = data_Swap;
+    cout << "SortedData_B: ";
+    for (int i = 1; i <= 20; i++) {
         cout << arr1[i] << " ";
     }
+    cout << endl;
+    print('A', c1, d1);
+    print('B', c2, d2);
 }
