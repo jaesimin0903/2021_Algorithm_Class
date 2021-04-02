fa4c94e5e95064fb2248e1ca2e5da9a7c191bed3
Jaemin Choi <a980903@naver.com>
2021-04-02 15:36:43 +0900

    4/2

--- 4_1.cpp
+++ 4_1.cpp
@@ -6,34 +6,67 @@ using namespace std;
 int data_Swap = 0;
 int compare = 0;
 int swap_Sum = 0;
+void SWAP(int* arr, int a, int b) {
+    int temp;
+    temp = arr[a];
+    arr[a] = arr[b];
+    arr[b] = temp;
+    data_Swap++;
+}
+int Partition(int* arr, int len) {
+    SWAP(arr, 0, rand() % len);
+    int temp = arr[0];
 
-int sum = 0;
-int partition(vector<int>& a, int l, int r) {
-    int i, j; int v;
-    if (r > l) {
-        v = a[l]; i = l; j = r + 1;
-        while (1) {
+    int low = 0;
+    int high = len;
 
-            while (i < a.size() - 1 && a[++i]);
-            while (a[--j] > v);
-            if (i >= j) break;
-            swap(a[i], a[j]);
-        }
-        swap(a[j], a[l]);
+    while (1) {
+        do {
+            low++;
+            compare++;
+        } while (low < len && arr[low] < temp);
+        do {
+            high--;
+            compare++;
+        } while (high > 0 && arr[high] > temp);
+
+        if (low > high) break;
+        SWAP(arr, low, high);
     }
-    return j;
+    SWAP(arr, 0, high);
+    return high;
+}
+void Quicksort(int* arr, int len) {
+    if (len <= 1) return;
 
+    int pivot = Partition(arr, len);
+    Quicksort(arr, pivot);
+    Quicksort(arr + pivot + 1, len - pivot - 1);
+}
+/*
+int Old_Partition(int* arr, int left, int right) {
+    int pivot = left;
+    while (left < right) {
+        while (arr[pivot] > arr[left] && left < right)
+            left++;
+        while (arr[pivot] <= arr[right] && left < right)
+            right--;
+        if (left < right)
+            SWAP(arr, left, right);
+    }
+    SWAP(arr, pivot, right);
+    return right;
 }
 
-void quicksort(vector<int>& a, int l, int r) {
-    int  j;
-    if (r > l) {
-
-        j = partition(a, l, r);
-        quicksort(a, l, j - 1);
-        quicksort(a, j + 1, r);
+void Old_Quicksort(int* arr, int left, int right) {
+    if (left < right) {
+        int pivot = Old_Partition(arr, left, right);
+        Old_Quicksort(arr, left, pivot);
+        Old_Quicksort(arr, pivot + 1, right);
     }
 }
+*/
+
 
 
 
@@ -48,19 +81,54 @@ int main()
     int N;
     cout << "N - ";
     cin >> N;
-    int j = N;
-    std::vector<int> arr;
-    arr.resize(N, 0);
+    int c1, c2, d1, d2;
+    int* arr = new int[N];
+    int* arr1 = new int[N];
+    int j = 0;
+    j = N;
     for (int i = 0; i < N; i++) {
-        arr.push_back(j);
+        arr[i] = j;
         j--;
     }
-    arr.erase(arr.begin(), arr.begin() + N);
 
-
-
-    quicksort(arr, 0, arr.size() - 1);
-    for (int i = 0;i < 20;i++) {
+    Quicksort(arr, N);
+    cout << "SortedData_A: ";
+    for (int i = 0; i < 20; i++) {
         cout << arr[i] << " ";
     }
+
+    for (int i = 1; i <= N; i++) {
+        arr1[i] = i;
+    }
+
+    c1 = compare;
+    d1 = data_Swap;;
+    cout << endl;
+
+    compare = 0;
+    data_Swap = 0;
+    //(unsigned(time(NULL)));
+
+    int r1, r2;
+    for (int i = 0; i < N; i++) {
+
+        r1 = 1 + rand() % N - 1;
+        r2 = 1 + rand() % N - 1;
+        SWAP(arr1, r1, r2);
+
+    }
+    Quicksort(arr1, N);
+    cout << "SortedData_B: ";
+    for (int i = 1; i <= 20; i++) {
+        cout << arr1[i] << " ";
+    }
+    c2 = compare;
+    d2 = data_Swap;;
+    cout << endl;
+    print('A', c1, d1);
+    print('B', c2, d2);
+    compare = 0;
+    data_Swap = 0;
+    cout << endl;
+    delete arr, arr1;
 }
---
+++ 4_2.cpp
@@ -0,0 +1,241 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>>
+
+using namespace std;
+int data_Swap = 0;
+int compare = 0;
+int swap_Sum = 0;
+
+void SWAP(int* arr, int a, int b) {
+    int temp;
+    temp = arr[a];
+    arr[a] = arr[b];
+    arr[b] = temp;
+
+}
+typedef int itemType;
+
+itemType b[100000];
+
+inline void swap(itemType a[], int i, int j) {
+    itemType t = a[i]; a[i] = a[j]; a[j] = t;
+    data_Swap++;
+};
+
+
+
+void makeHeap(itemType a[], int Root, int LastNode)
+
+{
+    int Parent, LeftSon, RightSon, Son, RootValue;
+    Parent = Root;
+    RootValue = a[Root];
+    LeftSon = 2 * Parent + 1;
+    RightSon = LeftSon + 1;
+    while (LeftSon <= LastNode) {
+        if (RightSon <= LastNode && a[LeftSon] < a[RightSon])
+        {
+            Son = RightSon; compare++;
+        }
+        else { Son = LeftSon; compare++; }
+        if (RootValue < a[Son]) {
+            compare++;
+            a[Parent] = a[Son];
+            Parent = Son;
+            LeftSon = Parent * 2 + 1;
+            RightSon = LeftSon + 1;
+        }
+        else break;
+    }
+    a[Parent] = RootValue;
+}
+
+void heapsort(itemType a[], int N)
+
+{
+
+
+
+    int i;
+
+    for (i = N / 2; i >= 1; i--)
+
+        makeHeap(a, i - 1, N - 1);
+
+    for (i = N - 1; i >= 1; i--)
+
+    {
+
+        swap(a, 0, i);
+
+        makeHeap(a, 0, i - 1);
+
+    }
+
+}
+
+
+
+void merge(int a[], int low, int mid, int high)
+
+{
+
+
+
+    int i, leftptr, rightptr, bufptr;
+
+    leftptr = low; rightptr = mid + 1; bufptr = low;
+
+
+
+    // 합병이 진행되고 있는 두 부분배열 모두에 아직 키가 남아있을 때 이들을 합병
+
+    while (leftptr <= mid && rightptr <= high)
+
+        if (a[leftptr] < a[rightptr])
+
+        {
+            b[bufptr++] = a[leftptr++]; data_Swap++;
+        }
+
+        else { b[bufptr++] = a[rightptr++]; data_Swap++; }
+
+
+
+    // 두 부분배열 중 한 개의 배열에만 키가 남았을 때 이 키들을 배열 b의
+
+    // 합병된 원소들 뒤에 그대로 복사
+
+    if (leftptr > mid)
+
+        for (i = rightptr; i <= high; i++)
+
+        {
+            b[bufptr++] = a[i]; compare++;
+        }
+
+    else
+
+        for (i = leftptr; i <= mid; i++)
+
+        {
+            b[bufptr++] = a[i]; compare++;
+        }
+
+
+
+    // 합병이 끝나면 b에 있는 정렬된 키들을 a에 옮긴다
+
+    for (i = low; i <= high; i++)
+
+        a[i] = b[i];
+
+}
+
+
+
+void mergesort(itemType a[], int l, int r)
+
+{
+
+    int mid;
+
+
+
+    if (l < r)
+
+    {
+
+        mid = (l + r) / 2; // 배열의 원소가 둘 이상이면
+
+
+
+        mergesort(a, l, mid); // 분할된 왼쪽 부분 배열에 대하여
+
+        // mergesort 순환호출
+
+
+
+
+
+        mergesort(a, mid + 1, r); // 분할된 오른쪽 부분 배열에 대하여
+
+        // mergesort 순환호출
+
+
+
+        merge(a, l, mid, r); // 정렬된 왼쪽과 오른쪽을 merge
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+void print(char A, int a, int b)
+{
+    cout << "Compare_Cnt_" << A << " " << a << ", " << "DataMove_Cnt_" << A << " " << b << endl;
+}
+
+
+int main()
+{
+    int N;
+    cout << "N - ";
+    cin >> N;
+    int c1, c2, d1, d2;
+    int* arr = new int[N];
+    int* arr1 = new int[N];
+    int j = 0;
+    j = N;
+    for (int i = 0; i < N; i++) {
+        arr[i] = j;
+        j--;
+    }
+
+    mergesort(arr, 0, N - 1);
+    cout << "SortedData_A: ";
+    for (int i = 0; i < 20; i++) {
+        cout << arr[i] << " ";
+    }
+
+    for (int i = 1; i <= N; i++) {
+        arr1[i] = i;
+    }
+
+    c1 = compare;
+    d1 = data_Swap;;
+    cout << endl;
+
+    compare = 0;
+    data_Swap = 0;
+
+    int r1, r2;
+    for (int i = 0; i < N; i++) {
+        r1 = 1 + rand() % N - 1;
+        r2 = 1 + rand() % N - 1;
+        SWAP(arr1, r1, r2);
+
+    }
+    mergesort(arr1, 0, N - 1);
+    cout << "SortedData_B: ";
+    for (int i = 1; i <= 20; i++) {
+        cout << arr1[i] << " ";
+    }
+    c2 = compare;
+    d2 = data_Swap;;
+    cout << endl;
+    print('A', c1, d1);
+    print('B', c2, d2);
+    compare = 0;
+    data_Swap = 0;
+    cout << endl;
+    delete arr, arr1;
+}
